a237e5ee3508b530660577d668f70ea6b781b105
chsd0 <serzer77@yandex.ru>
2024-09-30 13:11:55 +0300

    changed urls to backend deploy

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -5,7 +5,7 @@ import { validateEmail, validatePassword } from '../../utils/validation.js';
 import { Ajax } from '../../utils/ajax.js';
 import { toggleClasses } from '../../utils/toggleClasses.js';
 
-const ajax = new Ajax('http://127.0.0.1:8080/api/v1')
+const ajax = new Ajax('https://warm-coast-96136-d78b5be652e4.herokuapp.com/api/v1')
 
 /**
  * Renders the authentication template using Handlebars.
