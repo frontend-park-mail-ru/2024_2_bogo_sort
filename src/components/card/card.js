9720b2749b506f7d27912feeec72b32a18d02cdb
Alisa <askerrr2002@gmail.com>
2024-09-30 16:34:28 +0300

    added const + removed .precompiled.js

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -6,9 +6,10 @@
  * @param {string} title - Card title.
  * @param {string} price - Card price.
  * @param {string} image_url - Card image_url.
+ * @param {string} apiUrl - API URL.
  * @returns {string} The rendered HTML string of the card template.
  */
-export function renderCardTemplate(title, price, image_url) {
+export function renderCardTemplate(title, price, image_url, apiUrl) {
     const template = Handlebars.templates['card.hbs'];
-    return template({title, price, image_url});
+    return template({title, price, image_url, apiUrl});
 }
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -4,8 +4,10 @@ import { renderCardTemplate } from "../../components/card/card.js";
 import { Header } from "../../components/header/header.js";
 import { Ajax } from "../../utils/ajax.js";
 
+const apiUrl = 'https://warm-coast-96136-d78b5be652e4.herokuapp.com/api/v1';
+
 // const ajax = new Ajax('http://127.0.0.1:8080/api/v1');
-const ajax = new Ajax('https://warm-coast-96136-d78b5be652e4.herokuapp.com/api/v1')
+const ajax = new Ajax(apiUrl)
 
 /**
  * Represents the main page of the application.
@@ -54,7 +56,7 @@ export class MainPage {
         containerWrapper.appendChild(container);
 
         cards.forEach(element => {
-            container.innerHTML += renderCardTemplate(element.title, element.price, element.image_url);
+            container.innerHTML += renderCardTemplate(element.title, element.price, element.image_url, apiUrl);
         });
         this.#element.appendChild(containerWrapper);
     }
