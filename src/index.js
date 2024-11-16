966010deda067c7ce8e3053f32c8331955cff529
chsd0 <serzer77@yandex.ru>
2024-11-16 23:30:13 +0300

    TP-13f linter fixes

--- src/index.js
+++ src/index.js
@@ -60,11 +60,13 @@ router.addNewRouteWithRender('/search', renderSearch);
 function renderMain(main) {
     const mainPage = new MainPage();
 
+
     return mainPage.render(main);
 }
 
 function renderAdvert(main, advertId) {
     const advertPage = new AdvertPage();
+
 
     return advertPage.render(main, advertId);
 }
@@ -72,11 +74,13 @@ function renderAdvert(main, advertId) {
 function renderCreateAdvert(main) {
     const createPage = new CreateAdvertPage();
 
+
     return createPage.render(main);
 }
 
 function renderCart(main) {
     const cartPage = new CartPage();
+
 
     return cartPage.render(main);
 }
@@ -84,11 +88,13 @@ function renderCart(main) {
 function renderCategory(main, categoryId) {
     const categoryPage = new CategoryPage();
 
+
     return categoryPage.render(main, categoryId);
 }
 
 function renderAdvertEdit(main, advertId) {
     const advertEditPage = new AdvertEditPage();
+
 
     return advertEditPage.render(main, advertId);
 }
@@ -96,11 +102,13 @@ function renderAdvertEdit(main, advertId) {
 function renderUser(main, location) {
     const userPage = new UserPage();
 
+
     return userPage.render(main, location);
 }
 
 function renderSeller(main, sellerId) {
     const sellerPage = new SellerPage();
+
 
     return sellerPage.render(main, sellerId);
 }
