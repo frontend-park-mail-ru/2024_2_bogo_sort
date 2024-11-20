5381279c1affdc56eb71768008b1029eb42d7795
chsd0 <serzer77@yandex.ru>
2024-11-20 19:39:06 +0300

    TP-14c pr fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -24,15 +28,44 @@ module.exports = {
                     runtime: path.resolve(__dirname, 'src/utils/hbsHelpers')
                 }
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'sass-loader',
+                ]
+            }
             
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
         new CopyWebpackPlugin({
             patterns: [
                 { from: 'src/sw.js', to: ''},
             ],
         }),
     ],
-    mode: 'production'
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ]
+    },
+    mode: 'production',
+
+    devServer: {
+        historyApiFallback: true,
+        static: [
+            path.resolve(__dirname, '../src/dist'), path.resolve(__dirname, '../src')
+        ],
+        compress: true,
+        hot: true,
+        port: 8001,
+    }
 };
